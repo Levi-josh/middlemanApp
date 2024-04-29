5aa6409833ce8fbc638a516e62d5942f6ab652d7
Levi-josh <levijoshuakelly@gmail.com>
2024-04-29 20:28:14 -0200

    updated

--- src/Overview/Firstpage/Dashboard.tsx
+++ src/Overview/Firstpage/Dashboard.tsx
@@ -2,30 +2,30 @@ import { FaMoneyBill, FaMoneyBillTransfer, FaWallet } from "react-icons/fa6"
 
 const Dashboard = () => {
   return (
-    <div className="bg-black flex rounded-lg justify-between p-4 sm:p-6 flex-col h-full lg:w-1085 w-full lg:mt-6 ">
-      <h1>Overview</h1>
+    <div className="bg-black flex rounded-lg justify-evenly px-4 sm:p-6 flex-col h-full lg:w-1085 w-full lg:mt-6 ">
+      <h1 className="text-white sm:text-lg   font-semibold">Overview</h1>
       <div className="bg-black2 flex items-center gap-4 h-100025 p-2 sm:p-4 rounded-lg">
         <div className="flex flex-col justify-center bg-black w-full h-full p-1 sm:p-3 rounded-lg ">
-          <p>Balance</p>
-          <h1>$10,000</h1>
+          <p className="text-white sm:text-lg   ">Balance</p>
+          <p>$10,000</p>
         </div>
         <div className="flex flex-col justify-center bg-black2 w-full h-full hover:bg-black1 p-1 sm:p-3 rounded-lg ">
-          <p>Pending</p>
-          <h1>$100</h1>
+          <p className="text-white sm:text-lg  ">Pending</p>
+          <p>$100</p>
         </div>
       </div>
-      <h1>Money Transfer</h1>
+      <h1 className="text-white sm:text-lg   font-semibold">Money Transfer</h1>
       <div className="flex h-10004 justify-between">
-        <div className="h-full w-103 bg-black2 rounded-lg ">
-          <div><FaWallet/></div>
+        <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
+          <div className=" w-12 h-12 flex justify-center items-center rounded-full bg-black"><FaWallet/></div>
           <p>Withdraw</p>
         </div>
-        <div className="h-full w-103 bg-black2 rounded-lg">
-          <div><FaMoneyBill/></div>
+        <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
+          <div className=" w-12 h-12 flex justify-center items-center rounded-full bg-black"><FaMoneyBill/></div>
           <p>Deposit</p>
         </div>
-        <div className="h-full w-103 bg-black2 rounded-lg">
-          <div><FaMoneyBillTransfer/></div>
+        <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
+          <div className=" w-12 h-12 flex justify-center items-center rounded-full bg-black"><FaMoneyBillTransfer/></div>
           <p>Transfer</p>
         </div>
       </div>
--- src/Overview/Firstpage/Firstpage.tsx
+++ src/Overview/Firstpage/Firstpage.tsx
@@ -6,15 +6,15 @@ import pfp from '../../assets/IMG-20230507-WA0018.jpg'
 
 const Firstpage = () => {
   return (
-    <div className="w-full   lg:px-5   lg:w-1075 h-full">
+    <div className="w-full overflow-auto     lg:px-5   lg:w-1075 h-full">
     <Header/>
-    <div className="flex flex-col px-3 sm:px-4  lg:gap-6 py-16 lg:py-0 overflow-auto   lg:flex-row lg:justify-between  lg:items-center h-full lg:h-103 ">
+    <div className="flex flex-col px-3 justify-between sm:px-4  lg:gap-6 pt-14 lg:py-0  lg:flex-row lg:justify-between  lg:items-center h-full lg:h-103 ">
       <div className="lg:w-107 w-full rounded-lg h-102 lg:h-full  bg-black px-2 lg:px-3  lg:gap-2 flex-col flex justify-center items-start ">
         <div className="flex flex-row items-center gap-3 lg:gap-2 lg:flex-col">
-        <div className="lg:w-20 lg:h-20 w-14 h-14 overflow-hidden rounded-full bg-black2 outline outline-3 outline-purple">
+        <div className="sm:w-20 sm:h-20  w-14 h-14 overflow-hidden rounded-full bg-black2 outline outline-3 outline-purple">
           <img src={pfp}/>
         </div>
-        <p className="text-white">Levi joshua</p>
+        <p className="text-white sm:text-lg lg:text-xl font-bold">Levi joshua</p>
         </div>
         <div className="flex items-center gap-3">
           <p className=" text-white1 text-sm lg:text-base"><span className="font-bold">Invitation code</span>: hfjnnhghajkkghhssiij</p>
@@ -22,10 +22,10 @@ const Firstpage = () => {
         </div>
        
       </div> 
-     <div className="flex flex-row gap-3 items-center h-101 lg:justify-between lg:h-full lg:w-103 lg:flex-col">
-      <div className="flex w-full h-12 lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer px-2 rounded-lg"><p className="text-xs text-white">Create chat</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaMessage className="text-white"/></div></div>
-      <div className="flex w-full h-12 lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer   px-2 rounded-lg"><p className="text-xs text-white">Wallet ID</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaWallet className="text-white"/></div></div>
-      <div className="flex  w-full h-12 lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer   px-2 rounded-lg"><p className="text-xs text-white">Transfer</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaPiggyBank className="text-white"/></div></div>
+     <div className="flex flex-row gap-3  items-center h-1006 lg:justify-between lg:h-full lg:w-103 lg:flex-col">
+      <div className="flex w-full h-full lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer px-2 rounded-lg"><p className="text-xs text-white">Create chat</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaMessage className="text-white"/></div></div>
+      <div className="flex w-full h-full lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer   px-2 rounded-lg"><p className="text-xs text-white">Wallet ID</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaWallet className="text-white"/></div></div>
+      <div className="flex  w-full h-full lg:h-full justify-between items-center hover:transition-all hover:scale-x-105 bg-black hover:bg-black1 hover:cursor-pointer   px-2 rounded-lg"><p className="text-xs text-white">Transfer</p><div className="w-6 text-xs rounded-full flex justify-center items-center bg-purple h-6"><FaPiggyBank className="text-white"/></div></div>
      </div>
      <div className="lg:hidden h-106 bg-slate-800"><Dashboard/></div>
     </div>
