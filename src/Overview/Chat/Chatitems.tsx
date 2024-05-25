776bb18e6b080ba0caabbae78df5c0d4dae695a2
Levi-josh <levijoshuakelly@gmail.com>
2024-05-25 07:43:17 -0200

    updated

--- src/Overview/Chat/Chatitems.tsx
+++ src/Overview/Chat/Chatitems.tsx
@@ -10,10 +10,10 @@ const Chatitems = () => {
   {from:'josh',to:'vick',message:'see wetin go happen i go come your shop before evening make i see if the stuff go fit come out',msgTime:'just now'},
 ]
   return (
-    <div className="bg-black2 h-screen w-full ">
+    <div className="bg-black2 h-screen w-full  overflow-y-auto ">
     <ChatHeader/>
-    <div className={`pt-24 flex flex-col gap-5 px-4 w-full h-full     `}>
-      {chats.map(prev => <div className={`text-white h-auto block   w-auto max-w-56 bg-purple`}>
+    <div className={`pt-24 gap-5 px-4 w-full h-full      `}>
+      {chats.map(prev => <div className={`text-white ${prev.from=='josh'?'mr-0':'ml-0'} h-auto  mt-5 m-auto   w-auto max-w-56 bg-purple rounded-lg`}>
       <p>{prev.message}</p>
       <p className="">{prev.msgTime}</p>
     </div>)}
