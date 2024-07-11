a8e105d210ca845fb905e44dd0bbe9ce062d22c7
Levi-josh <levijoshuakelly@gmail.com>
2024-07-11 06:51:55 -0200

    updated

--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -1,5 +1,6 @@
 import { useState,FormEvent } from "react"
 import { useNavigate } from "react-router-dom"
+import midman from '../assets/IMG-20230507-WA0018.jpg'
 
 const Login = () => {
 const [text,setText]=useState("")
@@ -12,22 +13,53 @@ const handSubmit = (e:FormEvent<HTMLFormElement>)=>{
     }
 }
   return (
-    <div className="bg-black2 w-full h-screen flex justify-center items-center">
-        <form onSubmit={handSubmit} className="bg-black w-109 h-108 sm:w-1065 md:w-106 lg:w-104 lg:h-109 flex flex-col justify-center items-center ">
+    <div className="bg-black2 w-full h-full lg:flex justify-center overflow-auto  lg:justify-normal fixed ">
+        <form onSubmit={handSubmit} className="bg-black w-109 hidden h-108 sm:w-1065 md:w-106 lg:w-106 xl:w-104 lg:h-full lg:flex flex-col justify-center  px-4 ">
           <div className="flex flex-col gap-10 w-full justify-center items-center">
-            <input type="email" className="w-109 h-10 bg-black border-0.1   border-white  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1"  placeholder="Enter an email"  />
-            <input type="text" className="w-109 h-10 bg-black border border-solid  border-gray-700  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1" onChange={e=>{setText(e.target.value)}} value={text} placeholder="Enter a username"  />
-            <input type="password" className="w-109 h-10 bg-black border border-solid  border-gray-700  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1" placeholder="Enter a password"  />
+            <input type="email" className="w-full h-10 bg-black border-0.1   border-gray-700  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1"  placeholder="Enter an email"  />
+            <input type="text" className="w-full h-10 bg-black border border-solid  border-gray-700  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1" onChange={e=>{setText(e.target.value)}} value={text} placeholder="Enter a username"  />
+            <input type="password" className="w-full h-10 bg-black border border-solid  border-gray-700  text-white outline-none rounded-full placeholder:pl-1  pl-5 sm:py-1" placeholder="Enter a password"  />
           </div>
           <div className="flex flex-col gap-10 mt-7 w-full justify-center items-center">
             <div className=" w-full h-auto gap-3   flex items-center">
-              <hr className="   w-full"/>
+              <hr className="border-gray-700   w-full"/>
               <p className="text-white">Or</p>
-              <hr className="   w-full"/>
+              <hr className="w-full border-gray-700 "/>
             </div>
-            <div className="w-109 h-10 bg-black2 text-white flex justify-center items-center">Google</div>
+            <div className="w-full h-10 bg-black2 text-white flex justify-center items-center">Google</div>
           </div>
         </form>
+        <div>
+          <div className="w-full flex items-center  flex-col gap-10 bg-purple">
+            <div className="flex lg:flex-col items-center w-full">
+              <img src={midman} className="bg-no-repeat bg-cover bg-center rounded-full w-24 h-24"/>
+              <div className="flex flex-col items-center">
+              <h1 className="text-white">Welcome to The Middleman</h1>
+              <h2 className="text-white text-sm font-serif">Your Trusted Escrow Service for Secure Transactions...</h2>
+              </div>
+            </div>
+            <p className="text-white text-sm ">In the digital age, trust can be a tricky thing. Whether you're buying or selling goods and services online, the risk of scams and fraud can make even the simplest transactions nerve-wracking. That's where The Middleman comes in.</p>
+          </div>
+          <div>
+            <h1> How It Works </h1>
+            <ul>
+              <li>Buyer and Seller Agreement: The buyer and seller agree on the terms of the transaction.</li>
+              <li>Secure Payment: The buyer deposits the payment with The Middleman.</li>
+              <li> Product Delivery: The seller ships the product or provides the service.</li>
+              <li>Confirmation: The buyer confirms receipt and satisfaction with the item.</li> 
+              <li>Release of Funds: The Middleman releases the payment to the seller.</li> 
+            </ul>
+          </div>
+          <div>
+            <h1>Why Choose Us?</h1>
+            <p> Safety First: We hold the payment securely until the buyer confirms they've received the product or service.</p>
+            <p> Peace of Mind: Both buyers and sellers can engage in transactions with confidence, knowing their funds and products are protected.</p>
+            <p> User-Friendly: Our platform is designed to be intuitive and easy to use, making the process smooth and hassle-free.</p>
+          </div>
+          <div>
+            <p>Sign up now and experience the safest way to buy and sell online.</p>
+          </div>
+        </div>
     </div>
   )
 }
--- src/Overview/Firstpage/Dashboard.tsx
+++ src/Overview/Firstpage/Dashboard.tsx
@@ -5,11 +5,11 @@ const Dashboard = () => {
     <div className="bg-black flex rounded-lg justify-evenly px-4 sm:p-6 lg:p-3 flex-col h-full lg:w-1085 w-full lg:mt-6 ">
       <h1 className="text-white sm:text-lg   font-semibold">Overview</h1>
       <div className="bg-black2 flex items-center gap-4 h-100025 p-2 sm:p-4  lg:w-1083 rounded-lg">
-        <div className="flex flex-col lg:flex-row  gap-2 justify-center lg:items-center lg:justify-start  bg-black w-full h-full px-2 sm:px-3  rounded-lg ">
+        <div className="flex flex-col lg:flex-row  gap-2 justify-center lg:items-center lg:justify-between bg-black w-full h-full px-2 sm:px-3  rounded-lg ">
           <p className="text-white sm:text-lg font-semibold   ">Balance</p>
           <p className="text-sm text-white sm:text-base">$10,000</p>
         </div>
-        <div className="flex flex-col lg:flex-row gap-2 justify-center lg:items-center lg:justify-start  bg-black2 w-full h-full hover:bg-black1 px-2 sm:px-3 rounded-lg ">
+        <div className="flex flex-col lg:flex-row gap-2 justify-center lg:items-center lg:justify-between bg-black w-full h-full px-2 sm:px-3 rounded-lg ">
           <p className="text-white sm:text-lg font-semibold  ">Pending</p>
           <p className="text-sm text-white sm:text-base">$100</p>
         </div>
@@ -18,15 +18,15 @@ const Dashboard = () => {
       <div className="flex h-10004 justify-between">
         <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
           <div className=" w-16 h-16 sm:w-20 sm:h-20 lg:w-14 lg:h-14 flex justify-center items-center rounded-full bg-black"><FaWallet className="text-white text-lg sm:text-xl lg:text-base"/></div>
-          <p>Withdraw</p>
+          <p className="text-white">Withdraw</p>
         </div>
         <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
           <div className=" w-16 h-16 sm:w-20 sm:h-20 lg:w-14 lg:h-14 flex justify-center items-center rounded-full bg-black"><FaMoneyBill className="text-white text-lg sm:text-xl lg:text-base"/></div>
-          <p>Deposit</p>
+          <p className="text-white">Deposit</p>
         </div>
         <div className="h-full w-103 flex gap-3 justify-center items-center flex-col bg-black2 rounded-lg ">
           <div className=" w-16 h-16 sm:w-20 sm:h-20 lg:w-14 lg:h-14 flex justify-center items-center rounded-full bg-black"><FaMoneyBillTransfer className="text-white text-lg sm:text-xl lg:text-base"/></div>
-          <p>Transfer</p>
+          <p className="text-white">Transfer</p>
         </div>
       </div>
     </div>
