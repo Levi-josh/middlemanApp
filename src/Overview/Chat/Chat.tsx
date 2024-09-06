8abd0f923eb3e8a1c1fd2997c078833f8339b914
Levi-josh <levijoshuakelly@gmail.com>
2024-09-06 07:42:36 -0200

    updated

--- src/Overview/Chat/Chat.tsx
+++ src/Overview/Chat/Chat.tsx
@@ -53,9 +53,9 @@ const totalUnreadMessages = Chats?.reduce((total, chat) => {
   return (
     <div className="bg-black lg:fixed lg:right-0  pt-3 lg:h-screen z-50   lg:px-3 sm:pt-4 md:pt-5 h-full overflow-auto w-full   text-white lg:w-103 xl:w-1025 ">
     <Header2/> 
-    <div className="w-full text-center  bg-black2  pt-10 sm:pt-12 lg:pt-11 ">
-    {totalUnreadMessages && totalUnreadMessages>0  && <p className="py-2 lg:py-1">{`${totalUnreadMessages} unread message`}</p>}
-    </div>
+    {totalUnreadMessages && totalUnreadMessages>0  && <div className="w-full text-center  bg-black2  pt-10 sm:pt-12 lg:pt-11 ">
+    <p className="py-2 lg:py-1">{`${totalUnreadMessages} unread message`}</p>
+    </div>}
     { Chats && Chats.length>0?<Chatlist chatUsers={Chats} />:Chats?.length===0?<p className='text-white font-bold lg:mt-20 mt-28 md:mt-32 text-center '>No Chat yet!</p>:
     <div className="flex justify-center lg:mt-20 mt-28 md:mt-32  ">{errors?<div className='bg-purple px-3 py-1 sm:px-5 hover:cursor-pointer h-auto  rounded-full flex items-center gap-1'><FaArrowRotateLeft/><p >Retry</p></div>:<motion.div animate={{rotate:360}} transition={{duration:1,repeat: Infinity, ease: 'linear'}} className='' > <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M12 2V6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
