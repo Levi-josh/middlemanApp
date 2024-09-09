42dccaaa773e5ab924ec7c2017fe89353c5c6afe
Levi-josh <levijoshuakelly@gmail.com>
2024-09-09 16:12:14 -0200

    updated

--- src/Header/ChatHeader.tsx
+++ src/Header/ChatHeader.tsx
@@ -5,7 +5,7 @@ import { useChatContext } from '../Overview/Chat/ChatContext';
 const ChatHeader = (props:any) => {
   const { isfromChat } = useChatContext();
   return (
-    <div className="w-full z-10 bg-black lg:w-1069 xl:w-1074 px-3 sm:px-4 md:px-5 lg:px-3 flex items-center   justify-between fixed h-16 sm:h-20 md:h-24 lg:h-20">
+    <div className="w-full z-50 bg-black lg:w-1069 xl:w-1074 px-3 top-0  sm:px-4 md:px-5 lg:px-3 flex items-center   justify-between fixed h-16 sm:h-20 md:h-24 lg:h-20">
     <div className="flex items-center gap-5 sm:gap-10">
         <NavLink to={'/'}><FaArrowLeft className='text-white text-lg sm:text-xl md:text-2sxl lg:text-xl' onClick={isfromChat}/></NavLink>
         <div className="outline outline-2 outline-purple w-8 h-8 sm:w-11 sm:h-11 md:h-14 md:w-14 lg:w-11 lg:h-11 rounded-full overflow-hidden"><img src={props?.Dbmessages?.profilePic} /></div>
