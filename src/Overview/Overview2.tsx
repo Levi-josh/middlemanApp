0f3094e397c3a83ecae7c5d4c6a1560cb611747d
Levi-josh <levijoshuakelly@gmail.com>
2024-10-15 05:09:24 -0200

    updated

--- src/Overview/Overview2.tsx
+++ src/Overview/Overview2.tsx
@@ -1,21 +1,21 @@
 // import { useSearchParams} from "react-router-dom"
 import {  useEffect } from "react"
 import { useNavigate} from "react-router-dom"
-import { useDispatch } from 'react-redux';
-import { verifyAuth } from '../Feature/Redux';
-import { AppDispatch } from '../Feature/Store';
+// import { useDispatch } from 'react-redux';
+// import { verifyAuth } from '../Feature/Redux';
+// import { AppDispatch } from '../Feature/Store';
 
 const Overview2 = () => {
   // const [searchParams] = useSearchParams();
   // const loggedIn2 = searchParams.get('loggedIn');
-   const dispatch = useDispatch<AppDispatch>();
+  //  const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate()
   
 
   useEffect(() => {
     const storedDataString = localStorage.getItem('myData');
     if (storedDataString) {
-      dispatch(verifyAuth)
+      // dispatch(verifyAuth)
       const storedData = JSON.parse(storedDataString);
       if (storedData && storedData.expiration < Date.now()) {
            localStorage.removeItem('myData');
